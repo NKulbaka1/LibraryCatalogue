87e1908cc739ce4a0da23029395792005192e71c
NKulbaka <nikita.kulbaka@mail.ru>
2024-04-16 23:13:54 +0300

    Initial commit

---
+++ src/main/java/ru/kulbaka/libraryCatalogue/LibraryCatalogueApplication.java
@@ -0,0 +1,13 @@
+package ru.kulbaka.libraryCatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kulbaka/libraryCatalogue/LibraryCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kulbaka.libraryCatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
